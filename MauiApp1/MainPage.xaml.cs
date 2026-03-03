14c4f0c3b3f9d19f33aa375bc9abcceb20896f3b
pieczares <kupa.smierci123@gmail.com>
2026-03-03 23:09:33 +0100

    settings page

--- MauiApp1/MainPage.xaml.cs
+++ MauiApp1/MainPage.xaml.cs
@@ -21,27 +21,6 @@
             await Shell.Current.GoToAsync("RosaryMeditations");
         }
 
-        private async void Prayers_Tapped(object sender, TappedEventArgs e)
-        {
-            await Shell.Current.GoToAsync("Prayers");
-        }
 
-        private async void Retreat_Tapped(object sender, TappedEventArgs e)
-        {
-            await Shell.Current.GoToAsync("Retreat");
-        }
-        private async void OrderMass_Tapped(object sender, TappedEventArgs e)
-        {
-            await Shell.Current.GoToAsync("OrderMass");
-        }
-        private async void News_Tapped(object sender, TappedEventArgs e)
-        {
-            await Shell.Current.GoToAsync("News");
-        }
-
-        private async void Login_Tapped(object sender, TappedEventArgs e)
-        {
-            await Shell.Current.GoToAsync("Login");
-        }
     }
 }
--- MauiApp1/SettingsPage.xaml.cs
+++ MauiApp1/SettingsPage.xaml.cs
@@ -2,8 +2,46 @@ namespace MauiApp1;
 
 public partial class SettingsPage : ContentPage
 {
-	public SettingsPage()
+    private readonly AuthService _authService;
+    public SettingsPage(AuthService authService)
 	{
+        _authService = authService;
 		InitializeComponent();
-	}
+        IsLogged();
+    }
+
+    private async void IsLogged()
+    {
+        bool hasToken = await _authService.CheckAndSetTokenAsync();
+
+        if (hasToken)
+        {
+            LogIn.IsVisible = false;
+            LogOut.IsVisible = true;
+        }
+        else
+        {
+            LogIn.IsVisible = true;
+            LogOut.IsVisible = false;
+        }
+    }
+
+    private async void Login_Tapped(object sender, TappedEventArgs e)
+    {
+        await Shell.Current.GoToAsync("Login");
+    }
+
+    private async void Logout_Tapped(object sender, TappedEventArgs e)
+    {
+        bool IsLoggedout = await _authService.Logout();
+        if (IsLoggedout)
+        {
+            await DisplayAlertAsync("Informacja", "Konto zostało pomyślnie wylogowane", "OK");
+        }
+        else
+        {
+            await DisplayAlertAsync("BŁĄD", "Wylogowanie nie powiodło się, spróbuj ponownie", "OK");
+        }
+        IsLogged();
+    }
 }
