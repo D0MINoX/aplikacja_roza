01883bad2340419125d2aa66ec96d3502b881dcd
pieczares <kupa.smierci123@gmail.com>
2026-03-24 17:19:23 +0100

    skonczony login i dodany hamulec reczny

--- MauiApp1/Views/LoginPage.xaml.cs
+++ MauiApp1/Views/LoginPage.xaml.cs
@@ -5,17 +5,20 @@ namespace MauiApp1;
 public partial class LoginPage : ContentPage
 {
     private readonly AuthService _authService;
+    private bool _isBusy;
     public LoginPage(AuthService authService)
     {
         _authService = authService;
         CheckLoginStatus();
         InitializeComponent();
-
-
     }
+
     private async void OnLoginClicked(object sender, EventArgs e)
     {
+        if (_isBusy)
+            return;
 
+        _isBusy = true;
         try
         {
             bool success = await _authService.LoginAsync(EmailEntry.Text, PasswordEntry.Text);
@@ -23,13 +26,14 @@ public partial class LoginPage : ContentPage
             if (success)
             {
                 await DisplayAlertAsync("Sukces", "Zalogowano!", "OK");
-                string token;
+                _isBusy = false;
 
                 await Shell.Current.GoToAsync("Profile");
             }
             else
             {
                 await DisplayAlertAsync("Błąd", "Nieprawidłowe dane", "OK");
+                _isBusy = false;
             }
 
         }
@@ -41,7 +45,6 @@ public partial class LoginPage : ContentPage
     private async void CheckLoginStatus()
     {
         bool hasToken = await _authService.CheckAndSetTokenAsync();
-
         if (hasToken)
         {
             await Shell.Current.GoToAsync("Profile");
